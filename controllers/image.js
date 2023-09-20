3e905fe2795f5a0dd3477042a713feabfdffc266
Pranshu-Gupta <pranshugupta2910@gmail.com>
2023-09-21 00:01:22 +0530

    Test

--- controllers/image.js
+++ controllers/image.js
@@ -1,6 +1,6 @@
 const Clarifai = require('clarifai');
 const app = new Clarifai.App({
-  apiKey: process.env.API_CLARIFAI,
+  apiKey: '1acd4eaf5d88445aaa757614ffbfeff5',
 });
 
 const handleApiCall = (req, res) => {
